2140c8eade66171ef4bac61bbbdafb16e3fea931
Edwin Paul <saidarajab101@gmail.com>
2023-04-28 11:57:43 +0300

    Routes Lab

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route , Switch } from "react-router-dom";
 import App from "./components/App";
+import Home from "./components/Home";
+import Movies from "./components/Movies"
+import Directors from "./components/Directors";
+import Actors from "./components/Actors";
+import NavBar from "./components/NavBar";
+
 
 ReactDOM.render(
   <Router>
